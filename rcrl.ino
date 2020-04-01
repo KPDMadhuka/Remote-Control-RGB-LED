915292006cecf4830cbb54126c950d2dc5abbe54
K. P. D. Madhuka <kpdmadhuka@gmail.com>
2020-04-01 14:06:10 +0530

    Update rcrl.ino

--- rcrl.ino
+++ rcrl.ino
@@ -17,67 +17,57 @@ void setup()
 
 void loop() {
   if (irrecv.decode(&results)) {//irrecv.decode(&results) returns true if anything is recieved, and stores info in varible results
-    unsigned int value = results.value; //Get the value of results as an unsigned int, so we can use switch case
+    long int value = results.value; //Get the value of results as an unsigned int, so we can use switch case
     Serial.println(value);
     switch (value) {
-      case 20655: 
+      //button 1      
+      case 33444015: 
       digitalWrite(4, 255);
       digitalWrite(6, 0);
       digitalWrite(9, 0);
-      delay(500);
-      digitalWrite(4, LOW);
-      digitalWrite(6, LOW);
-      digitalWrite(9, LOW);
         break;
-      
-      case 63495:
-      digitalWrite(4, 0);
-      digitalWrite(6, 0);
-      digitalWrite(9, 255);
-      delay(500);
-      digitalWrite(4, LOW);
-      digitalWrite(6, LOW);
-      digitalWrite(9, LOW);
-        break;
-      
-      case 55335:
+        
+      //button 2
+      case 33478695:
       digitalWrite(4, 0);
       digitalWrite(6, 255);
       digitalWrite(9, 0);
-      delay(500);
+        break;
+        
+      //button 3
+      case 33486855:
+      digitalWrite(4, 0);
+      digitalWrite(6, 0);
+      digitalWrite(9, 255);
+        break;
+        
+      //button 4
+      case 33435855:
+      digitalWrite(4, 100);
+      digitalWrite(6, 0);
+      digitalWrite(9, 100);
+        break;
+        
+      //button 5 
+      case 33468495:
+      digitalWrite(4, 100);
+      digitalWrite(6, 100);
+      digitalWrite(9, 0);
+        break;
+        
+      //button 6
+      case 33452175:
+      digitalWrite(4, 0);
+      digitalWrite(6, 100);
+      digitalWrite(9, 100);
+        break;
+        
+      //button Power OFF        
+      case 33441975:
       digitalWrite(4, LOW);
       digitalWrite(6, LOW);
       digitalWrite(9, LOW);
         break;
-      
-      case 12495:
-      digitalWrite(4, HIGH);
-      digitalWrite(6, HIGH);
-      digitalWrite(9, HIGH);
-      delay(500);
-      digitalWrite(4, LOW);
-      digitalWrite(6, LOW);
-      digitalWrite(9, LOW);
-        break;
-
-      case 2103:
-      digitalWrite(4, 245);
-      digitalWrite(6, 242);
-      digitalWrite(9, 66);
-      delay(500);
-      digitalWrite(4, LOW);
-      digitalWrite(6, LOW);
-      digitalWrite(9, LOW);
-        break;
-      
-      case 55:
-      digitalWrite(4, 245);
-      digitalWrite(6, 242);
-      digitalWrite(9, 66);
-      delay(500);
-      digitalWrite(4, LOW);
-      digitalWrite(6, LOW);
-      digitalWrite(9, LOW);
     }
     
     
